5e06feae303afaee96f5308174dee298bcdc8168
rohanrj3296 <147172259+rohanrj3296@users.noreply.github.com>
2025-01-16 20:42:47 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 go build -o Bread_Butter.exe ./cmd/web/. 
-Bread_Butter.exe -dbname=bread_butter_bookings -dbuser=postgres -dbpass=ROHANjd@3296 -cache=false -production=false 
+Bread_Butter.exe -dbname=bread_butter_bookings -dbuser=postgres -dbpass=password -cache=false/true -production=false/true
